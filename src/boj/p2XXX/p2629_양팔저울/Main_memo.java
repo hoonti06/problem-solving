ce565f09bb3ded1313778537ee4852c4f28a8544
hoonti06 <hoonti06@gmail.com>
2021-06-15 02:25:06 +0900

    code : 파일명 변경

--- src/boj/p2XXX/p2629_양팔저울/Main_memo.java
+++ src/boj/p2XXX/p2629_양팔저울/Main_memo.java
@@ -3,32 +3,29 @@ package boj.p2XXX.p2629_양팔저울;
 import java.io.*;
 import java.util.*;
 
-public class Main_recur {
+public class Main_memo {
     static final int OFFSET = 15010;
     static int N, M, weightSum;
     static int[] weight;
-    static int[][] dp = new int[31][OFFSET * 2];
+    static boolean[][] memo = new boolean[31][OFFSET * 2];
 
-    static int go(int cnt, int num) {
-//        if (cnt >= N) return false;
-//        if (num < 0 || num > OFFSET + weightSum) return false;
-//        if (cnt == 0 && num == 0) return true;
-//        if (cnt < 0) return false;
-//
-////        dp[cnt][num] |= d
-//        if (dp[cnt][num]) return dp[cnt][num];
-//
-//        dp[cnt][num] |= go(cnt - 1, num - weight[cnt]);
-//        dp[cnt][num] |= go(cnt - 1, num + weight[cnt]);
-//        dp[cnt][num] |= go(cnt - 1, num);
-        return dp[cnt][num];
+    static boolean go(int cnt, int num) {
+        if (cnt >= N) return false;
+        if (num < 0 || num > OFFSET + weightSum) return false;
+        if (cnt == 0 && num == 0) return true;
+        if (cnt < 0) return false;
+
+        if (memo[cnt][num]) return memo[cnt][num];
+
+        memo[cnt][num] |= go(cnt - 1, num - weight[cnt]);
+        memo[cnt][num] |= go(cnt - 1, num + weight[cnt]);
+        memo[cnt][num] |= go(cnt - 1, num);
+        return memo[cnt][num];
     }
 
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("in.txt"));
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-//        for (int i = 0; i < 31; i++) Arrays.fill(dp[i], )
 
         N = Integer.parseInt(in.readLine());
         StringTokenizer st = new StringTokenizer(in.readLine(), " ");
@@ -43,19 +40,21 @@ public class Main_recur {
 
         M = Integer.parseInt(in.readLine());
         st = new StringTokenizer(in.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             int input = Integer.parseInt(st.nextToken());
-            String res = "N ";
+            String res = "N";
             if (input <= weightSum) {
                 for (int j = 1; j <= N; j++) {
-                    if (dp[j][OFFSET + input] == 1) {
-                        res = "Y ";
+                    if (memo[j][OFFSET + input]) {
+                        res = "Y";
                         break;
                     }
                 }
             }
-            System.out.print(res);
+            sb.append(res).append(" ");
         }
+        System.out.println(sb);
     }
 }
 
