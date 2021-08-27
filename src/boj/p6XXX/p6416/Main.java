45cd94a26b276bf5545c955733f90dc16e818aad
hoonti06 <hoonti06@gmail.com>
2021-08-27 20:38:00 +0900

    fix : 틀린 문제 재 풀이

--- src/boj/p6XXX/p6416/Main.java
+++ src/boj/p6XXX/p6416/Main.java
@@ -4,29 +4,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    static int N;
-    static Map<Integer, List<Integer>> edgeMap;
-    static Set<Integer> toSet;
+    static Map<Integer, List<Integer>> edges;
     static Map<Integer, Integer> indegree;
     static Set<Integer> nodes;
-
-
-    static int findRoot() {
-        int root = -1;
-        int rootCnt = 0;
-        for(int node : nodes) {
-            if (indegree.get(node) == null) {
-                root = node;
-                rootCnt++;
-            }
-            else if (indegree.get(node) > 1) {
-                return -1;
-            }
-        }
-        if (rootCnt != 1) return -1;
-        return root;
-    }
 
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("in.txt"));
@@ -34,78 +14,97 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         boolean isFin = false;
-        int k = 1;
+        int cnt = 1;
         while (true) {
-            toSet = new HashSet<>();
             indegree = new HashMap<>();
             nodes = new HashSet<>();
-            edgeMap = new HashMap<>();
-            while (true) {
-//            StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+            edges = new HashMap<>();
+
+            boolean testcaseFin = false;
+            while (!testcaseFin) {
                 String line = in.readLine();
-                if (line.length() == 0) break;
-                String[] split = line.split(" ");
-                if (split[0].equals("-1") && split[1].equals("-1")) {
+                if (line == null) {
                     isFin = true;
                     break;
+                } else if (line.length() == 0) {
+                    continue;
                 }
 
-                int halfLen = split.length / 3 + 1;
-                boolean next = false;
-                for (int i = 0; i < halfLen; i++) {
-                    int from = Integer.parseInt(split[i*3]);
-                    int to = Integer.parseInt(split[i*3+1]);
-                    if (from == 0 && to == 0) break;
-
-                    List<Integer> list = edgeMap.getOrDefault(from, new ArrayList<>());
-                    list.add(to);
-                    indegree.put(to, indegree.getOrDefault(to, 0) + 1);
-                    edgeMap.put(from, list);
-                    toSet.add(to);
-                    nodes.add(from);
-                    nodes.add(to);
-                }
-            }
-
-            boolean isTree = true;
-
-            int root = -1;
-            if (nodes.size() > 0) {
-                root = findRoot();
-                if (root == -1) isTree = false;
-            }
-
-            if (root != -1) {
-                Set<Integer> visitSet = new HashSet<>();
-                Queue<Integer> q = new ArrayDeque<>();
-                q.offer(root);
-                while (!q.isEmpty()) {
-                    int from = q.poll();
-                    if (visitSet.contains(from)) {
-                        isTree = false;
+                StringTokenizer st = new StringTokenizer(line, " ");
+                do {
+                    int from = Integer.parseInt(st.nextToken());
+                    int to = Integer.parseInt(st.nextToken());
+                    if (from < 0 && to < 0) {
+                        testcaseFin = true;
+                        isFin = true;
+                        break;
+                    } else if (from == 0 && to == 0) {
+                        testcaseFin = true;
                         break;
                     }
 
-                    visitSet.add(from);
-                    List<Integer> fromEdge = edgeMap.get(from);
-                    if (fromEdge == null) continue;
-                    for (int i = 0; i < fromEdge.size(); i++) {
-                        int to = fromEdge.get(i);
-                        q.offer(to);
+                    indegree.put(to, indegree.getOrDefault(to, 0) + 1);
+
+                    List<Integer> list = edges.getOrDefault(from, new ArrayList<>());
+                    list.add(to);
+                    edges.put(from, list);
+
+                    nodes.add(from);
+                    nodes.add(to);
+
+                } while (st.hasMoreTokens());
+            }
+            if (isFin) {
+                break;
+            }
+
+            boolean isTree = true;
+            int root = -1;
+            if (nodes.size() > 0) {
+                int rootCnt = 0;
+                for(int node : nodes) {
+                    int indegreeCnt = indegree.getOrDefault(node, 0);
+                    if (indegreeCnt == 0) {
+                        root = node;
+                        rootCnt++;
+                    } else if (indegreeCnt > 1) {
+                        isTree = false;
+                        break;
                     }
                 }
-                if (visitSet.size() != nodes.size())
+                if (rootCnt != 1) {
                     isTree = false;
+                }
+
+                if (isTree) {
+                    Set<Integer> visited = new HashSet<>();
+                    Queue<Integer> q = new ArrayDeque<>();
+                    q.offer(root);
+                    while (!q.isEmpty()) {
+                        int from = q.poll();
+                        if (visited.contains(from)) {
+                            isTree = false;
+                            break;
+                        }
+                        visited.add(from);
+
+                        for (Integer to : edges.getOrDefault(from, new ArrayList<>())) {
+                            q.offer(to);
+                        }
+                    }
+                    if (visited.size() != nodes.size()) {
+                        isTree = false;
+                    }
+                }
             }
 
             sb.append("Case ")
-                    .append(k)
+                    .append(cnt)
                     .append(" is ")
                     .append(isTree? "" : "not ")
                     .append("a tree.\n");
-            k++;
-            if (isFin) break;
+            cnt++;
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
